8fc030b566d5e91a0e599b093beeccec2725e653
Manas Chopra <74074241+manas95826@users.noreply.github.com>
2023-10-09 23:44:11 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from PIL import Image
+import pytesseract
+from pytesseract import Output
+
+st.title("OCR with Bounding Box and Layout Analysis")
+
+# Upload an image or PDF file
+uploaded_file = st.file_uploader("Upload an image or PDF file", type=["png", "jpg", "jpeg", "pdf"])
+
+if uploaded_file is not None:
+    # Display the uploaded image
+    if uploaded_file.type.startswith("image"):
+        image = Image.open(uploaded_file)
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+    elif uploaded_file.type == "application/pdf":
+        st.warning("PDF support is under development. Please upload an image instead.")
+
+    # Perform OCR
+    if st.button("Run OCR"):
+        text = pytesseract.image_to_string(image, output_type=Output.DICT)
+        st.write("Extracted Text:")
+        st.write(text['text'])
+
+        # Visualize bounding boxes (optional)
+        for i, word in enumerate(text['text']):
+            left, top, width, height = text['left'][i], text['top'][i], text['width'][i], text['height'][i]
+            st.image(image.crop((left, top, left + width, top + height)))
+
+        # Add layout analysis or table detection here
+        st.write("Layout Analysis / Table Detection Results:")
+
+# Example: You can integrate Hugging Face models for advanced layout analysis or table detection.
+# You would need to install the required Hugging Face libraries and use the model accordingly.
+# Example: https://huggingface.co/models?pipeline_tag=table-question-answering
+
